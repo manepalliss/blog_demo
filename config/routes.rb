0553e56fc842f9e6ab68d2d213d06954565ab506
manepalliss <saisantoshmanepalli@gmail.com>
2022-08-23 15:16:57 +0530

    upgrate search and setting up admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  authenticated :user, ->(user) { user.admin? } do 
+    get 'admin', to: 'admin#index'
+    get 'admin/posts'
+    get 'admin/comments'
+    get 'admin/users'
+    get 'admin/post/:id', to: 'admin#show_post', as: 'admin_post' 
+  end
+
   get 'search', to: 'search#index'
+
   get 'users/profile'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
@@ -11,7 +20,6 @@ Rails.application.routes.draw do
   resources :posts do 
     resources :comments 
   end
-
 
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,23 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: 'mss@gmail.com', password: 'password', password_confirmation: 'password')
+User.create(email: 'mss@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+            name: 'mss',
+            role: User.roles[:admin])
+User.create(email: 'saisantoshmanepalli@gmail.com',
+            password: 'SSAU#199998',
+            password_confirmation: 'SSAU#199998',
+            name: 'santosh')
 
 10.times do |x|
-  Post.create(title: "Title #{x}", body: "Body #{x} Words go here Idk", user_id: User.first.id)
+  post = Post.create(title: "Title #{x}",
+                     body: "Body #{x} Words go here Idk",
+                     user_id: User.first.id)
+
+  5.times do |y|
+    post.comments.create(body: "Comment #{y}",
+                   user_id: User.second.id)
+  end
 end
